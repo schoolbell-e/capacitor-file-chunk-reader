b850100dd70c97908ec59f551cff48fad0a33366
Daniel Kim <khg1945@yahoo.com>
2023-09-21 06:38:15 +0900

    add content uri approach

--- android/src/main/java/com/schoolbell_e/plugins/file_chunk_reader/FileChunkReader.java
+++ android/src/main/java/com/schoolbell_e/plugins/file_chunk_reader/FileChunkReader.java
@@ -8,4 +8,31 @@ public class FileChunkReader {
         Log.i("Echo", value);
         return value;
     }
+
+    public String readChunkForContentUri () {
+        ContentResolver contentResolver = getContentResolver(); // Replace with your context or activity
+
+        Uri contentUri = Uri.parse("content://your_content_provider/your_resource");
+        String[] projection = {MediaStore.Images.Media.DATA}; // Replace with your desired columns
+        Cursor cursor = contentResolver.query(contentUri, projection, null, null, null);
+        
+        String data = "";
+        if (cursor != null) {
+            if (cursor.moveToPosition(startIndex)) {
+                // Loop through the range
+                for (int i = startIndex; i <= endIndex; i++) {
+                    // Read data from the cursor
+                    data += cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA));
+        
+                    // Move to the next position
+                    if (!cursor.moveToNext()) {
+                        break; // End of the range
+                    }
+                }
+            }
+            cursor.close(); // Close the cursor when done
+        }        
+        return data;
+        
+    }
 }
