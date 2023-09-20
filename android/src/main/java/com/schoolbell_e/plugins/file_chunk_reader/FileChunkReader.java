42e65643142a6be369f19ae8695ff9215b660d2d
Daniel Kim <khg1945@yahoo.com>
2023-09-21 06:40:52 +0900

    add stat

--- android/src/main/java/com/schoolbell_e/plugins/file_chunk_reader/FileChunkReader.java
+++ android/src/main/java/com/schoolbell_e/plugins/file_chunk_reader/FileChunkReader.java
@@ -1,7 +1,10 @@
 package com.schoolbell_e.plugins.file_chunk_reader;
 
 import android.util.Log;
-
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 public class FileChunkReader {
 
     public String echo(String value) {
@@ -34,5 +37,36 @@ public class FileChunkReader {
         }        
         return data;
         
+    }
+
+    public Void getStatFromContentUri () {
+        // Replace "your_content_uri" with the actual content URI you want to retrieve information for.
+        Uri contentUri = Uri.parse("content://your_content_provider/your_resource");
+
+        ContentResolver contentResolver = getContentResolver(); // Replace with your context or activity
+
+        // Define the columns you want to retrieve
+        String[] projection = {
+            MediaStore.Files.FileColumns.SIZE, // File size in bytes
+            MediaStore.Files.FileColumns.DATE_MODIFIED // Last modification time in seconds since epoch
+        };
+
+        Cursor cursor = contentResolver.query(contentUri, projection, null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            int sizeIndex = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.SIZE);
+            int mtimeIndex = cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.DATE_MODIFIED);
+
+            long fileSize = cursor.getLong(sizeIndex);
+            long lastModifiedTimeInSeconds = cursor.getLong(mtimeIndex);
+
+            // Convert the last modified time to milliseconds
+            long lastModifiedTimeMillis = lastModifiedTimeInSeconds * 1000;
+
+            // Now you have the file size and last modification time
+            // You can use fileSize and lastModifiedTimeMillis as needed
+
+            cursor.close(); // Close the cursor when done
+        }        
     }
 }
